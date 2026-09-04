1130da3b77a9bba0628c5fcb3ad525dc91dfdc74
dondavidruiz <dondavidruiz@gmail.com>
2026-09-04 11:06:38 +0200

    Add files via upload

--- firebase-config.js
+++ firebase-config.js
@@ -1,4 +1,10 @@
-var FIREBASE_CONFIG = {
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
   apiKey: "AIzaSyDPIELHhLz4LH7EKUHweYkcMmt8lKT4N1k",
   authDomain: "intercambios2627.firebaseapp.com",
   projectId: "intercambios2627",
@@ -7,4 +13,5 @@ var FIREBASE_CONFIG = {
   appId: "1:581281112333:web:f62e64f6f074be59959044"
 };
 
-var ADMIN_EMAIL = "admin@intercambios-crfptic.local";
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
